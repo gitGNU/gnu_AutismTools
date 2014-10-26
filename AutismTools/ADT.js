5f5f85e65c0878b5e5c746ea161a61aeb6e6bdcd
René Mérou <h@es.gnu.org>
2014-10-26 13:51:05 +0100

    Added the functionality to modify the actual funcionalities on the map

--- AutismTools/ADT.js
+++ AutismTools/ADT.js
@@ -21,6 +21,12 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
+var box=0;
+var localbox=0;
+var localboxchanging=0;
+var x=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+var xy=[x];//doble dimensional array with all the data for all the boxes.
+
 
 function showForm(functionality) {
   document.getElementById('selectedFunctionality').value=functionality;/*leaves only the name, everything is cleaned*/
@@ -47,6 +53,8 @@ function showForm(functionality) {
   document.getElementById('frequencyOfFunctionality5').value="normal";
   document.getElementById('frequencyOfFunctionality6').value="normal";
   document.getElementById('frequencyOfFunctionality7').value="normal";
+//  document.getElementById('professionalEffectOfFunctionality1').value=functionality ;
+  document.getElementById('StimulusLevelOfFunctionality1').value="";
   document.getElementById('block2').style.display='none';/*Show only fist block*/
   document.getElementById('block3').style.display='none';
   document.getElementById('block4').style.display='none';
@@ -55,6 +63,106 @@ function showForm(functionality) {
   document.getElementById('block7').style.display='none';
   document.getElementById('form').style.display='block';/*activate the form*/
 }
+
+
+
+function showForm2(functionality,thislocalbox) {
+  localboxchanging=thislocalbox;
+  document.getElementById('selectedFunctionality_2').value=xy[thislocalbox,0]; 
+  document.getElementById('twoExtraWords_2').value=xy[thislocalbox,1];
+  document.getElementById('twoExtraWords_2').style.display=xy[thislocalbox,25]; 
+  document.getElementById('degreeOfFunctionality1_2').value=xy[thislocalbox,2];
+
+  document.getElementById('degreeOfFunctionality2_2').value=xy[thislocalbox,3];
+  document.getElementById('degreeOfFunctionality3_2').value=xy[thislocalbox,4];
+  document.getElementById('degreeOfFunctionality4_2').value=xy[thislocalbox,5];
+  document.getElementById('degreeOfFunctionality5_2').value=xy[thislocalbox,6];
+  document.getElementById('degreeOfFunctionality6_2').value=xy[thislocalbox,7];
+  document.getElementById('degreeOfFunctionality7_2').value=xy[thislocalbox,8];
+  document.getElementById('pleasureOfFunctionality1_2').value=xy[thislocalbox,9];
+  document.getElementById('pleasureOfFunctionality2_2').value=xy[thislocalbox,10];
+  document.getElementById('pleasureOfFunctionality3_2').value=xy[thislocalbox,11];
+  document.getElementById('pleasureOfFunctionality4_2').value=xy[thislocalbox,12];
+  document.getElementById('pleasureOfFunctionality5_2').value=xy[thislocalbox,13];
+  document.getElementById('pleasureOfFunctionality6_2').value=xy[thislocalbox,14];
+  document.getElementById('pleasureOfFunctionality7_2').value=xy[thislocalbox,15];
+  document.getElementById('frequencyOfFunctionality1_2').value=xy[thislocalbox,16];
+  document.getElementById('frequencyOfFunctionality2_2').value=xy[thislocalbox,17];
+  document.getElementById('frequencyOfFunctionality3_2').value=xy[thislocalbox,18];
+  document.getElementById('frequencyOfFunctionality4_2').value=xy[thislocalbox,19];
+  document.getElementById('frequencyOfFunctionality5_2').value=xy[thislocalbox,20];
+  document.getElementById('frequencyOfFunctionality6_2').value=xy[thislocalbox,21];
+  document.getElementById('frequencyOfFunctionality7_2').value=xy[thislocalbox,22];
+  document.getElementById('professionalEffectOfFunctionality1_2').value=xy[localbox,23] ;
+  document.getElementById('StimulusLevelOfFunctionality1_2').value=xy[localbox,24];
+  document.getElementById('block2_2').style.display=xy[thislocalbox,26];
+  document.getElementById('block3_2').style.display=xy[thislocalbox,27];
+  document.getElementById('block4_2').style.display=xy[thislocalbox,28];
+  document.getElementById('block5_2').style.display=xy[thislocalbox,29];
+  document.getElementById('block6_2').style.display=xy[thislocalbox,30];
+  document.getElementById('block7_2').style.display=xy[thislocalbox,31];
+  
+
+  document.getElementById('form2').style.display='block';/*activate the form*/
+}
+
+
+function activateTwoExtraWords_2() {
+  if (document.getElementById('twoExtraWords_2').style.display=='block') {
+    document.getElementById('twoExtraWords_2').style.display='none';
+  } else {
+    document.getElementById('twoExtraWords_2').style.display='block';
+  }
+}
+function activateblock2_2() {
+  if (document.getElementById('block2_2').style.display=='block') {
+    document.getElementById('block2_2').style.display='none';
+  } else {
+    document.getElementById('block2_2').style.display='block';
+  }
+}
+function activateblock3_2() {
+  if (document.getElementById('block3_2').style.display=='block') {
+    document.getElementById('block3_2').style.display='none';
+  } else {
+    document.getElementById('block3_2').style.display='block';
+  }
+}
+function activateblock4_2() {
+  if (document.getElementById('block4_2').style.display=='block') {
+    document.getElementById('block4_2').style.display='none';
+  } else {
+    document.getElementById('block4_2').style.display='block';
+  }
+}
+function activateblock5_2() {
+  if (document.getElementById('block5_2').style.display=='block') {
+    document.getElementById('block5_2').style.display='none';
+  } else {
+    document.getElementById('block5_2').style.display='block';
+  }
+}
+function activateblock6_2() {
+  if (document.getElementById('block6_2').style.display=='block') {
+    document.getElementById('block6_2').style.display='none';
+  } else {
+    document.getElementById('block6_2').style.display='block';
+  }
+}
+function activateblock7_2() {
+  if (document.getElementById('block7_2').style.display=='block') {
+    document.getElementById('block7_2').style.display='none';
+  } else {
+    document.getElementById('block7_2').style.display='block';
+  }
+}
+
+
+
+
+
+
+
 function activateTwoExtraWords() {
   if (document.getElementById('twoExtraWords').style.display=='block') {
     document.getElementById('twoExtraWords').style.display='none';
@@ -105,8 +213,47 @@ function activateblock7() {
   }
 }
 function addFunctionality() {
-  var item='<spam class="'+pleasureOfFunctionality1.value+frequencyOfFunctionality1.value+'">'+degreeOfFunctionality1.value+'</spam>';
-  if (degreeOfFunctionality2.value!="") {
+  box=box+1;
+  localbox=box;
+
+  xy[localbox,0]= selectedFunctionality.value;
+  xy[localbox,1]= twoExtraWords.value;
+  xy[localbox,2]= degreeOfFunctionality1.value;
+  xy[localbox,3]= degreeOfFunctionality2.value;
+  xy[localbox,4]= degreeOfFunctionality3.value;
+  xy[localbox,5]= degreeOfFunctionality4.value;
+  xy[localbox,6]= degreeOfFunctionality5.value;
+  xy[localbox,7]= degreeOfFunctionality6.value;
+  xy[localbox,8]= degreeOfFunctionality7.value;
+  xy[localbox,9]= pleasureOfFunctionality1.value;
+  xy[localbox,10]= pleasureOfFunctionality2.value;
+  xy[localbox,11]=pleasureOfFunctionality3.value;
+  xy[localbox,12]=pleasureOfFunctionality4.value ;
+  xy[localbox,13]=pleasureOfFunctionality5.value ;
+  xy[localbox,14]=pleasureOfFunctionality6.value ;
+  xy[localbox,15]=pleasureOfFunctionality7.value ;
+  xy[localbox,16]=frequencyOfFunctionality1.value;
+  xy[localbox,17]=frequencyOfFunctionality2.value;
+  xy[localbox,18]=frequencyOfFunctionality3.value;
+  xy[localbox,19]=frequencyOfFunctionality4.value ;
+  xy[localbox,20]=frequencyOfFunctionality5.value ;
+  xy[localbox,21]=frequencyOfFunctionality6.value ;
+  xy[localbox,22]=frequencyOfFunctionality7.value ;
+  xy[localbox,23]=professionalEffectOfFunctionality1.value ;
+  xy[localbox,24]=StimulusLevelOfFunctionality1.value ;
+  xy[localbox,25]=document.getElementById('twoExtraWords').style.display;
+  xy[localbox,26]=document.getElementById('block2').style.display;
+  xy[localbox,27]=document.getElementById('block3').style.display;
+  xy[localbox,28]=document.getElementById('block4').style.display;
+  xy[localbox,29]=document.getElementById('block5').style.display;
+  xy[localbox,30]=document.getElementById('block6').style.display;
+  xy[localbox,31]=document.getElementById('block7').style.display;
+  
+  
+  var item='<a href="javascript:;" onclick="showForm2('+"'"+selectedFunctionality.value+"'"+','+localbox+');">&nbsp;&nbsp;'+localbox+'&nbsp;&nbsp;</a>';
+  item=item+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+  item=item+'<spam class="'+pleasureOfFunctionality1.value+frequencyOfFunctionality1.value+'" >'+degreeOfFunctionality1.value+'</spam>';
+  if (degreeOfFunctionality2.value!="" && document.getElementById('block2').style.display =='block') {
     item=item+'<spam class="'+pleasureOfFunctionality2.value+frequencyOfFunctionality2.value+'">'+degreeOfFunctionality2.value+'</spam>';
   }
   if (degreeOfFunctionality3.value!="") {
@@ -128,8 +275,89 @@ function addFunctionality() {
     item=item+"<br />"+'<spam class="'+pleasureOfFunctionality1.value+'rare">'+selectedFunctionality.value;
   }
   if (twoExtraWords.value!="") {item=item+"<br />"+twoExtraWords.value;}
-  item='<div item="yes">'+item+'</spam></div>';
+  item='<div item="yes" id='+localbox+'>'+item+'</spam></div>';
   probando='#'+professionalEffectOfFunctionality1.value+"a"+StimulusLevelOfFunctionality1.value+'';
   $(probando).append(item);
   document.getElementById('form').style.display='none';
 }
+
+function changeFunctionality() {
+  //box=box+1;
+   
+  elemento = document.getElementById(localboxchanging);
+  padre = elemento.parentNode;
+  padre.removeChild(elemento); 
+  
+  localbox=localboxchanging;
+
+  xy[localbox,0]= selectedFunctionality_2.value;
+  xy[localbox,1]= twoExtraWords_2.value;
+  xy[localbox,2]= degreeOfFunctionality1_2.value;
+  xy[localbox,3]= degreeOfFunctionality2_2.value;
+  xy[localbox,4]= degreeOfFunctionality3_2.value;
+  xy[localbox,5]= degreeOfFunctionality4_2.value;
+  xy[localbox,6]= degreeOfFunctionality5_2.value;
+  xy[localbox,7]= degreeOfFunctionality6_2.value;
+  xy[localbox,8]= degreeOfFunctionality7_2.value;
+  xy[localbox,9]= pleasureOfFunctionality1_2.value;
+  xy[localbox,10]= pleasureOfFunctionality2_2.value;
+  xy[localbox,11]=pleasureOfFunctionality3_2.value;
+  xy[localbox,12]=pleasureOfFunctionality4_2.value ;
+  xy[localbox,13]=pleasureOfFunctionality5_2.value ;
+  xy[localbox,14]=pleasureOfFunctionality6_2.value ;
+  xy[localbox,15]=pleasureOfFunctionality7_2.value ;
+  xy[localbox,16]=frequencyOfFunctionality1_2.value;
+  xy[localbox,17]=frequencyOfFunctionality2_2.value;
+  xy[localbox,18]=frequencyOfFunctionality3_2.value;
+  xy[localbox,19]=frequencyOfFunctionality4_2.value ;
+  xy[localbox,20]=frequencyOfFunctionality5_2.value ;
+  xy[localbox,21]=frequencyOfFunctionality6_2.value ;
+  xy[localbox,22]=frequencyOfFunctionality7_2.value ;
+  xy[localbox,23]=professionalEffectOfFunctionality1_2.value ;
+  xy[localbox,24]=StimulusLevelOfFunctionality1_2.value ;
+  xy[localbox,25]=document.getElementById('twoExtraWords_2').style.display;
+  xy[localbox,26]=document.getElementById('block2_2').style.display;
+  xy[localbox,27]=document.getElementById('block3_2').style.display;
+  xy[localbox,28]=document.getElementById('block4_2').style.display;
+  xy[localbox,29]=document.getElementById('block5_2').style.display;
+  xy[localbox,30]=document.getElementById('block6_2').style.display;
+  xy[localbox,31]=document.getElementById('block7_2').style.display;
+
+
+ 
+  var item='<a href="javascript:;" onclick="showForm2('+"'"+selectedFunctionality_2.value+"'"+','+localbox+');">&nbsp;&nbsp;'+localbox+'&nbsp;&nbsp;</a>';
+  item=item+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+  item=item+'<spam class="'+pleasureOfFunctionality1_2.value+frequencyOfFunctionality1_2.value+'" >'+degreeOfFunctionality1_2.value+'</spam>';
+  if (degreeOfFunctionality2_2.value!="" && document.getElementById('block2_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality2_2.value+frequencyOfFunctionality2_2.value+'">'+degreeOfFunctionality2_2.value+'</spam>';
+  }
+  if (degreeOfFunctionality3_2.value!="" && document.getElementById('block3_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality3_2.value+frequencyOfFunctionality3_2.value+'">'+degreeOfFunctionality3_2.value+'</spam>';
+  }
+  if (degreeOfFunctionality4_2.value!="" && document.getElementById('block4_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality4_2.value+frequencyOfFunctionality4_2.value+'">'+degreeOfFunctionality4_2.value+'</spam>';
+  }
+  if (degreeOfFunctionality5_2.value!="" && document.getElementById('block5_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality5_2.value+frequencyOfFunctionality5_2.value+'">'+degreeOfFunctionality5_2.value+'</spam>';
+  }
+  if (degreeOfFunctionality6_2.value!="" && document.getElementById('block6_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality6_2.value+frequencyOfFunctionality6_2.value+'">'+degreeOfFunctionality6_2.value+'</spam>';
+  }
+  if (degreeOfFunctionality7_2.value!="" && document.getElementById('block7_2').style.display =='block') {
+    item=item+'<spam class="'+pleasureOfFunctionality7_2.value+frequencyOfFunctionality7_2.value+'">'+degreeOfFunctionality7_2.value+'</spam>';
+  }
+  if (selectedFunctionality_2.value!="") {
+    item=item+"<br />"+'<spam class="'+pleasureOfFunctionality1_2.value+'rare">'+selectedFunctionality_2.value;
+  }
+  if (twoExtraWords_2.value!=""  && document.getElementById('twoExtraWords_2').style.display =='block') {item=item+"<br />"+twoExtraWords_2.value;}
+  item='<div item="yes" id='+localbox+'>'+item+'</spam></div>';
+  probando='#'+professionalEffectOfFunctionality1_2.value+"a"+StimulusLevelOfFunctionality1_2.value+'';
+  $(probando).append(item);
+  
+ 
+  
+  document.getElementById('form2').style.display='none';
+}
+
+
+
